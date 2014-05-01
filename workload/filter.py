8bd0cc38512c5e53d143890951cf1794a72ba5be
Danny Milsom <danny.milsom@cgi.com>
2014-05-01 13:11:38 +0000

    Fixes #4394 - New workload plugin which adds some piecharts to milestone pages.
    * Uses jqPlot like burn down charts.
    * Adds scripts and DOM elements via filter stream.
    * Queries DB after page rendering to reduce load time, and sends data as JSON via AJAX.
    * Uses colours from define-layout.css but might still need some tweeking.
    * Pie charts include hyperlinks to ticket query / manage hours page.
    * No charts should appear until tickets are assigned to the milestone.
    * If response to XMLHttpRequest is bad, removes the charts and shows a error message similar to burn down charts.
    * Tested with PostgreSQL and SQLite.
    
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@23589 484df14f-75d2-4b3f-8ba0-490e05069a01

---
+++ workload/filter.py
@@ -0,0 +1,237 @@
+from trac.config import Option
+from trac.core import Component, TracError, implements
+from trac.resource import ResourceNotFound
+from trac.ticket.model import Milestone
+from trac.util.presentation import to_json
+from trac.web import ITemplateStreamFilter
+from trac.web.api import IRequestHandler
+from trac.web.chrome import ITemplateProvider, add_script, add_stylesheet, add_script_data
+
+from genshi.filters.transform import Transformer
+from genshi.builder import tag
+import pkg_resources
+import re
+
+class Workload(Component):
+
+    implements(IRequestHandler, ITemplateStreamFilter, ITemplateProvider)
+
+    user_limit = Option('workload', 'workload_user_limit', 9)
+
+    # IRequestHandler methods
+
+    def match_request(self, req):
+        """
+        Match against a path, and check that the milestone idenfied exists via
+        _get_milestone().
+        """
+
+        if req.path_info.startswith("/ajax/workload"):
+            try:
+                return self._get_milestone(req.args['id'])
+            except KeyError:
+                return False
+
+    def process_request(self, req):
+        """
+        Send a JSON response containing workload data to a XMLHttpRequest. Any 
+        other type of request raise a TracError.
+        """
+
+        if not req.get_header('X-Requested-With') == 'XMLHttpRequest':
+            raise TracError("We only accept XMLHttpRequests to this URL")
+
+        milestone = req.args['id']
+        ticket_count = self._get_open_ticket_count(milestone)
+        hour_count = self._get_remaining_hours(milestone)
+        closed_count = self._get_closed_ticket_count(milestone)
+        logged_count = self._get_hours_logged(milestone)
+
+        data = [self._limit_user_data(d) for d in (ticket_count, hour_count, 
+                                                   closed_count, logged_count)]
+
+        req.send(to_json(data), 'text/json')
+
+    # ITemplateStreamFilter method
+
+    def filter_stream(self, req, method, filename, stream, data):
+        """
+        We add JavaScript files and other DOM elements via the filter stream, 
+        but actually query the DB and parse the data after the initial page 
+        load via AJAX for a page load performance boost. See IRequestHandler 
+        methods.
+
+        We only add these scripts and elements if the milestone has a minimum 
+        of atleast one ticket.
+
+        We also only render the completed work DOM elements if a minimum of 
+        one ticket has been closed.
+        """
+
+        if req.path_info.startswith('/milestone/'):
+
+            milestone = self._get_milestone(req.args['id'])
+            if milestone and self._milestone_has_ticket(milestone.name):
+
+                add_script(req, 'common/js/jqPlot/jquery.jqplot.js')
+                add_script(req, 'common/js/jqPlot/excanvas.min.js')
+                add_script(req, 'common/js/jqPlot/plugins/jqplot.pieRenderer.min.js')
+                add_script(req, 'common/js/jqPlot/plugins/jqplot.highlighter.min.js')
+                add_stylesheet(req, 'common/js/jqPlot/jquery.jqplot.css')
+                add_script(req, 'workload/js/workload.js')
+                add_script_data(req, {'milestone_name': milestone.name})
+
+                workload_tag = tag(
+                    tag.h2("Remaining Work"),
+                    tag.div(
+                        tag.div(id_='milestone-workload',
+                                class_='milestone-info span6 center',
+                                style="display:inline;"
+                        ),
+                        tag.div(id_='milestone-workload-hours',
+                                class_='milestone-info span6 center',
+                                style="display:inline;"
+                        ),
+                    id_="workload-charts",
+                    class_="row-fluid"
+                    )
+                )
+                stream = stream | Transformer("//*[@id='field-analysis']").after(workload_tag)
+
+                if self._milestone_has_closed_ticket(milestone.name):
+
+                    workdone_tag = tag(
+                        tag.h2("Completed Work"),
+                        tag.div(
+                            tag.div(id_='milestone-workdone',
+                                    class_='milestone-info span6 center',
+                                    style="display:inline;"
+                            ),
+                            tag.div(id_='milestone-workdone-hours',
+                                    class_='milestone-info span6 center',
+                                    style="display:inline;"
+                            ),
+                        id_="workdone-charts",
+                        class_="row-fluid"
+                        ),
+                    )
+                    stream = stream | Transformer("//*[@id='workload-charts']").after(workdone_tag)
+
+        return stream
+
+    # ITemplateProvider methods
+
+    def get_htdocs_dirs(self):
+        return [('workload', pkg_resources.resource_filename(__name__,
+                                                                'htdocs'))]
+
+    def get_templates_dirs(self):
+        return [pkg_resources.resource_filename(__name__, 'templates')]
+
+    # Private methods
+
+    def _get_milestone(self, milestone):
+        """Returns a milestone instance if one exists, or None if it
+        does not."""
+
+        try:
+            m = Milestone(self.env, milestone)
+        except ResourceNotFound:
+            m = None
+        return m
+
+    def _milestone_has_ticket(self, milestone):
+        db = self.env.get_read_db()
+        cursor = db.cursor()
+        cursor.execute("""SELECT id FROM ticket 
+                          WHERE milestone=%s
+                          LIMIT 1""",
+                          (milestone,))
+        return cursor.fetchone()
+
+    def _milestone_has_closed_ticket(self, milestone):
+        db = self.env.get_read_db()
+        cursor = db.cursor()
+        cursor.execute("""SELECT id FROM ticket 
+                          WHERE milestone=%s
+                            AND status = 'closed'
+                          LIMIT 1""",
+                          (milestone,))
+        return cursor.fetchone()
+
+    def _get_open_ticket_count(self, milestone):
+        db = self.env.get_read_db()
+        cursor = db.cursor()
+        cursor.execute("""SELECT owner, COUNT(*)
+                          FROM ticket 
+                          WHERE milestone=%s
+                            AND status != 'closed'
+                          GROUP BY owner
+                          ORDER BY COUNT(*) DESC""",
+                          (milestone,))
+        return cursor.fetchall()
+
+    def _get_remaining_hours(self, milestone):
+        db = self.env.get_read_db()
+        cursor = db.cursor()
+        # can't cast as int due to decimal remaininghours values
+        cursor.execute("""SELECT t.owner, SUM(CAST(c.value as numeric))
+                        FROM ticket as t
+                        JOIN ticket_custom AS c
+                            ON c.ticket = t.id
+                        WHERE t.milestone=%s
+                            AND c.name=%s
+                            AND t.status!='closed'
+                        GROUP BY t.owner
+                        ORDER BY SUM(CAST(c.value as numeric)) DESC""",
+                  (milestone,"remaininghours"))
+        return cursor.fetchall()
+
+    def _get_closed_ticket_count(self, milestone):
+        db = self.env.get_read_db()
+        cursor = db.cursor()
+        cursor.execute("""SELECT owner, COUNT(*) FROM ticket 
+                          WHERE milestone=%s
+                            AND status = 'closed'
+                          GROUP BY owner
+                          ORDER BY COUNT(*) DESC""",
+                          (milestone,))
+        return cursor.fetchall()
+
+    def _get_hours_logged(self, milestone):
+        db = self.env.get_read_db()
+        cursor = db.cursor()
+        cursor.execute("""SELECT tt.worker, SUM(tt.seconds_worked) 
+                          FROM ticket_time as tt 
+                          JOIN ticket as t
+                            ON t.id = tt.ticket
+                          WHERE t.milestone=%s
+                          GROUP BY tt.worker
+                          ORDER BY SUM(tt.seconds_worked) DESC""",
+                          (milestone,))
+
+        return [(worker, (logged/60/60)) for worker, logged in cursor]
+
+    def _limit_user_data(self, data, limit=None):
+        """Summarises user data to limit the number of individual users 
+        referenced. This is mainly a convience to keep the UI nice and tidy.
+
+        By default this uses the limit values set by the self.user_limit 
+        config option.
+
+        Unassigned tickets have a empty string value in the ticket table, 
+        so we find this tuple if present and change the label to 'unassigned.
+        """
+
+        data = [(owner, work) if owner else ('unassigned', work)
+                              for owner, work in data]
+
+        if limit is None:
+            limit = self.user_limit
+
+        if len(data) > limit:
+            other = [('other', sum(i[1] for i in data[limit:]))]
+        else:
+            other = []
+
+        return data[:limit] + other
---
+++ workload/htdocs/js/workload.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+
+  var spinner_markup = "<i class='workload-spinner icon-spinner \
+                        icon-spin icon-2x color-muted-dark'></i>"
+  var workload_selectors = "#milestone-workload, #milestone-workload-hours, \
+                            #milestone-workdone, #milestone-workdone-hours"
+  $(workload_selectors).append(spinner_markup);
+
+  $.ajax({
+    type:"GET",
+    data: {'id': milestone_name},
+    url: window.tracBaseUrl + "ajax/workload",
+    success: function(data) {
+      $(".workload-spinner").remove();
+      draw_piechart(data[0], 'milestone-workload');
+      draw_piechart(data[1], 'milestone-workload-hours');
+      draw_piechart(data[2], 'milestone-workdone');
+      draw_piechart(data[3], 'milestone-workdone-hours');
+    },
+    error: function(data) {
+      $(".workload-spinner, #milestone-workload-hours, #milestone-workdone-hours").remove()
+      $("#milestone-workdone, #milestone-workload").removeClass("span6 center")
+                                                   .addClass("span12")
+                                                   .html("<i class='icon-info-sign'></i> Failed to retrieve workload data.")
+    }
+  });
+
+  function draw_piechart(data, chart) {
+    var plot1 = jQuery.jqplot (chart, [data], 
+      { 
+        seriesDefaults: {
+          renderer: jQuery.jqplot.PieRenderer, 
+          rendererOptions: {
+            showDataLabels: true,
+            dataLabels: 'value', // can use 'percent' too
+          }
+        }, 
+        legend: { show:true, location: 'e' },
+        grid: {
+          background: "#F5F5F5",
+          shadow: false,
+          borderWidth: 0
+        },
+        seriesColors: [ "#23932C", "#dbb800", "#A94442", "#e9ba9e", "#CEF5D1",
+                        "#9966FF", "#ab8800", "#DFEEFD", "#80ff80", "#F2DEDE"]
+      }
+    );
+  };
+
+  // set the correct title attribute when user hovers over segment
+  $(workload_selectors).bind('jqplotDataHighlight', function(ev, seriesIndex, pointIndex, data) {
+    var $this = $(this);
+    console.log(data)
+    $this.css('cursor', 'pointer');
+    title = data[0] + ": " + data[1]
+    if ($this.attr('id') == "milestone-workload") {
+      $this.attr('title', title + " open tickets");
+    }
+    else if ($this.attr('id') == "milestone-workload-hours") {
+      $this.attr('title', title + " hours remaining");
+    }
+    else if ($this.attr('id') == "milestone-workdone") {
+      $this.attr('title', title + " tickets closed");
+    }
+    else if ($this.attr('id') == "milestone-workdone-hours") {
+      $this.attr('title', title + " hours logged");
+    }
+    else {
+      $this.attr('title', title);
+    }
+  });
+
+  // remove title attribute on mouseout
+  $(workload_selectors).bind('jqplotDataUnhighlight', function(ev, seriesIndex, pointIndex, data) {
+    var $this = $(this);
+    $this.attr('title',""); 
+  });
+
+  // set link when user clicks on segment
+  $(workload_selectors).bind('  jqplotDataClick', function(ev, seriesIndex, pointIndex, data) {
+    if (data[0] == "unassigned") {
+      var query = "/project1/query?owner=&milestone=" + milestone_name;
+    } else {
+      var query = "/project1/query?owner=~" + data[0] + "&milestone=" + milestone_name;
+    }
+
+    if ($(this).attr('id') == "milestone-workdone") {
+      var query = query + "&statusgroup=Closed";
+    }
+    else if ($(this).attr('id') == "milestone-workdone-hours") {
+      // send user to manage hours page if they click on the hours logged chart
+      var query = "/project1/hours?col=seconds_worked&col=worker&col=time_started&worker_filter=" + data[0];
+    }
+    else {
+      var query = query + "&statusgroup=Active";
+    }
+    window.location = (query)
+  });
+
+
+})
